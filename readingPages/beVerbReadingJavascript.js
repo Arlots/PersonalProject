d77225bd7839417a9ab5a9c270a118b54319492c
Arlots <ethan.jennings092@gmail.com>
2023-05-09 09:28:41 +0900

    added code to swap out level 2 reading passage and questions when code is exectued.

--- readingPages/beVerbReadingJavascript.js
+++ readingPages/beVerbReadingJavascript.js
@@ -23,6 +23,8 @@ function readingSwap(){
    const readingQuestion3 = document.getElementById("readingQuestion3");
 
    const questionLevel1_1 = "1.	ステーブンの妻の名前はなんですか？";
+   const questionLevel2_1 = "1.	Cardiologistはなんですか？";
+
    const readingComp1A = document.getElementById("readingComp1A");
    const readingComp1B = document.getElementById("readingComp1B");
    const readingComp1C = document.getElementById("readingComp1C");
@@ -33,9 +35,12 @@ function readingSwap(){
    const answerLabel1C = document.getElementById("answerLabel1C");
    const answerLabel1D = document.getElementById("answerLabel1D");
 
-   const questionLevel1_1Choices = ["a. Seth", "b. Samantha", "c. Sherry", "d. Steve"];
-   
+   const questionLevel1_1Choices = ["a. Seth", "b. Samantha", "c. Sherry", "d. Steven"];
+   const questionLevel2_1Choices = ["a. 心臓専門医", "泌尿器科医", "目医者", "皮膚科医"];
+
    const questionLevel1_2 = "2.	Sethは何年生ですか？";
+   const questionLevel2_2 = "2.	誰がbubblyという性格を持っているのですか？";
+
    const readingComp2A = document.getElementById("readingComp2A");
    const readingComp2B = document.getElementById("readingComp2B");
    const readingComp2C = document.getElementById("readingComp2C");
@@ -47,8 +52,12 @@ function readingSwap(){
    const answerLabel2D = document.getElementById("answerLabel2D");
 
    const questionLevel1_2Choices = ["a. 5年生", "b. 6年生", "c. 2年生", "d. 4年生"];
+   const questionLevel2_2Choices = ["a. Samantha", "b. Seth", "c. Steven", "d. Sherry"];
+
 
    const questionLevel1_3 = "3.	Sherryの職業はなんですか？";
+   const questionLevel2_3 = "3.	StevenとSherryは何歳ですか？";
+
    const readingComp3A = document.getElementById("readingComp3A");
    const readingComp3B = document.getElementById("readingComp3B");
    const readingComp3C = document.getElementById("readingComp3C");
@@ -60,6 +69,7 @@ function readingSwap(){
    const answerLabel3D = document.getElementById("answerLabel3D");
 
    const questionLevel1_3Choices = ["a. doctor", "b. teacher", "c. bakery", "d. housewife"];
+   const questionLevel2_3Choices = ["a. 42と39", "b. 39と40", "c. 32と49", "d. 44と39"];
 
    switch (selectedLevel.value) {
         case "Level1":
@@ -77,7 +87,7 @@ function readingSwap(){
             answerLabel1B.innerText = questionLevel1_1Choices[1];
             readingComp1C.setAttribute("value", "sherry");
             answerLabel1C.innerText = questionLevel1_1Choices[2];
-            readingComp1D.setAttribute("value", "steve");
+            readingComp1D.setAttribute("value", "steven");
             answerLabel1D.innerText = questionLevel1_1Choices[3];
 
             readingQuestion2.innerText = questionLevel1_2;
@@ -107,10 +117,40 @@ function readingSwap(){
             readingLine3.innerText = level2Line3;
             readingLine4.innerText = level2Line4;
             readingAudio.setAttribute("src", "./BeVerbBeginnerRecoring_Level2.wav")
+
+            readingQuestion1.innerText = questionLevel2_1;
+            readingComp1A.setAttribute("value", "cardiologist");
+            answerLabel1A.innerText = questionLevel2_1Choices[0];
+            readingComp1B.setAttribute("value", "urologist");
+            answerLabel1B.innerText = questionLevel2_1Choices[1];
+            readingComp1C.setAttribute("value", "eyedoctor");
+            answerLabel1C.innerText = questionLevel2_1Choices[2];
+            readingComp1D.setAttribute("value", "dermatologist");
+            answerLabel1D.innerText = questionLevel2_1Choices[3];
+
+            readingQuestion2.innerText = questionLevel2_2;
+            readingComp2A.setAttribute("value", "samantha");
+            answerLabel2A.innerText = questionLevel2_2Choices[0];
+            readingComp2B.setAttribute("value", "seth");
+            answerLabel2B.innerText = questionLevel2_2Choices[1];
+            readingComp2C.setAttribute("value", "steven");
+            answerLabel2C.innerText = questionLevel2_2Choices[2];
+            readingComp2D.setAttribute("value", "sherry");
+            answerLabel2D.innerText = questionLevel2_2Choices[3];
+
+            readingQuestion3.innerText = questionLevel2_3;
+            readingComp3A.setAttribute("value", "42_39");
+            answerLabel3A.innerText = questionLevel2_3Choices[0];
+            readingComp3B.setAttribute("value", "39_40");
+            answerLabel3B.innerText = questionLevel2_3Choices[1];
+            readingComp3C.setAttribute("value", "32_49");
+            answerLabel3C.innerText = questionLevel2_3Choices[2];
+            readingComp3D.setAttribute("value", "44_39");
+            answerLabel3D.innerText = questionLevel2_3Choices[3];
             break;
    }
 
-}
+};
 
 function loadDefaultReading() {
     const selectedLevel = document.getElementById("levelSelect");
@@ -126,12 +166,85 @@ function loadDefaultReading() {
     const level1Line4 = "Their children are Samantha and Seth. Seth is a sixth grader. Samantha is a junior high school student."
  
 
+    const readingQuestion1 = document.getElementById("readingQuestion1");
+    const readingQuestion2 = document.getElementById("readingQuestion2");
+    const readingQuestion3 = document.getElementById("readingQuestion3");
+ 
+    const questionLevel1_1 = "1.	ステーブンの妻の名前はなんですか？";
+    const readingComp1A = document.getElementById("readingComp1A");
+    const readingComp1B = document.getElementById("readingComp1B");
+    const readingComp1C = document.getElementById("readingComp1C");
+    const readingComp1D = document.getElementById("readingComp1D");
+ 
+    const answerLabel1A = document.getElementById("answerLabel1A");
+    const answerLabel1B = document.getElementById("answerLabel1B");
+    const answerLabel1C = document.getElementById("answerLabel1C");
+    const answerLabel1D = document.getElementById("answerLabel1D");
+ 
+    const questionLevel1_1Choices = ["a. Seth", "b. Samantha", "c. Sherry", "d. Steve"];
+    
+    const questionLevel1_2 = "2.	Sethは何年生ですか？";
+    const readingComp2A = document.getElementById("readingComp2A");
+    const readingComp2B = document.getElementById("readingComp2B");
+    const readingComp2C = document.getElementById("readingComp2C");
+    const readingComp2D = document.getElementById("readingComp2D");
+ 
+    const answerLabel2A = document.getElementById("answerLabel2A");
+    const answerLabel2B = document.getElementById("answerLabel2B");
+    const answerLabel2C = document.getElementById("answerLabel2C");
+    const answerLabel2D = document.getElementById("answerLabel2D");
+ 
+    const questionLevel1_2Choices = ["a. 5年生", "b. 6年生", "c. 2年生", "d. 4年生"];
+ 
+    const questionLevel1_3 = "3.	Sherryの職業はなんですか？";
+    const readingComp3A = document.getElementById("readingComp3A");
+    const readingComp3B = document.getElementById("readingComp3B");
+    const readingComp3C = document.getElementById("readingComp3C");
+    const readingComp3D = document.getElementById("readingComp3D");
+ 
+    const answerLabel3A = document.getElementById("answerLabel3A");
+    const answerLabel3B = document.getElementById("answerLabel3B");
+    const answerLabel3C = document.getElementById("answerLabel3C");
+    const answerLabel3D = document.getElementById("answerLabel3D");
+ 
+    const questionLevel1_3Choices = ["a. doctor", "b. teacher", "c. bakery", "d. housewife"];
+
     readingLine1.innerText = level1Line1;
     readingLine2.innerText = level1Line2;
     readingLine3.innerText = level1Line3;
     readingLine4.innerText = level1Line4;
     readingAudio.setAttribute("src", "./BeVerbBeginnerRecoring_Level1.wav")
-}
+
+    readingQuestion1.innerText = questionLevel1_1;
+    readingComp1A.setAttribute("value", "seth");
+    answerLabel1A.innerText = questionLevel1_1Choices[0];
+    readingComp1B.setAttribute("value", "samantha");
+    answerLabel1B.innerText = questionLevel1_1Choices[1];
+    readingComp1C.setAttribute("value", "sherry");
+    answerLabel1C.innerText = questionLevel1_1Choices[2];
+    readingComp1D.setAttribute("value", "steve");
+    answerLabel1D.innerText = questionLevel1_1Choices[3];
+
+    readingQuestion2.innerText = questionLevel1_2;
+    readingComp2A.setAttribute("value", "fifth");
+    answerLabel2A.innerText = questionLevel1_2Choices[0];
+    readingComp2B.setAttribute("value", "sixth");
+    answerLabel2B.innerText = questionLevel1_2Choices[1];
+    readingComp2C.setAttribute("value", "second");
+    answerLabel2C.innerText = questionLevel1_2Choices[2];
+    readingComp2D.setAttribute("value", "fourth");
+    answerLabel2D.innerText = questionLevel1_2Choices[3];
+
+    readingQuestion3.innerText = questionLevel1_3;
+    readingComp3A.setAttribute("value", "doctor");
+    answerLabel3A.innerText = questionLevel1_3Choices[0];
+    readingComp3B.setAttribute("value", "teacher");
+    answerLabel3B.innerText = questionLevel1_3Choices[1];
+    readingComp3C.setAttribute("value", "bakery");
+    answerLabel3C.innerText = questionLevel1_3Choices[2];
+    readingComp3D.setAttribute("value", "housewife");
+    answerLabel3D.innerText = questionLevel1_3Choices[3];
+};
 
 loadDefaultReading();
 
